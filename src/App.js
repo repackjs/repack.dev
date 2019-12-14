eb60bdd7e367b9a03fc6bc42381dd898b871ed64
George Karalis <15313012+thorbust@users.noreply.github.com>
2019-12-14 20:45:02 +0200

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-headline App-headline--big">repack</h1>
+        <p className="App-text">Repackage and optimize JavaScript bundles</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
